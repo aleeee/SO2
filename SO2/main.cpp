27c78508208685b54f55414915fa44aacc59cf17
Tomasz Kiszka <tomek.kiszka@gmail.com>
2013-05-22 19:13:12 +0200

    Initial commit.

---
+++ SO2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SO2
+//
+//  Created by Tomasz on 22.05.2013.
+//  Copyright (c) 2013 Tomasz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
